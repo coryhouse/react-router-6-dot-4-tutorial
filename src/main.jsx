7cb605059b4fb6edb88f00102a4623150bea38e0
Cory House <housecor@gmail.com>
2022-09-05 20:13:36 -0500

    Finish tutorial

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,10 @@ import Root, {
 } from "./routes/root";
 import Index from "./routes/index";
 import ErrorPage from "./error-page";
-import Contact, { loader as contactLoader } from "./routes/contact";
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from "./routes/contact";
 import EditContact, { action as editAction } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
 
@@ -22,28 +25,34 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         loader={rootLoader}
         action={rootAction}
       >
-        <Route index element={<Index />} />
-        <Route
-          path="contacts/:contactId"
-          element={<Contact />}
-          loader={contactLoader}
-        />
-        <Route
-          path="contacts/:contactId/edit"
-          element={<EditContact />}
-          loader={contactLoader}
-          action={editAction}
-        />
-        <Route
-          path="contacts/:contactId/destroy"
-          action={destroyAction}
-          errorElement={
-            <div>
-              Oops! There was an error deleting the user.{" "}
-              <Link to="/contacts/:contactId">Back to the contact</Link>
-            </div>
-          }
-        />
+        {/* Wrapping in a route with no path so we can declare a common error page 
+        for all the child routes below. 
+        This way the left panel nav continues to display after an error. */}
+        <Route errorElement={<ErrorPage />}>
+          <Route index element={<Index />} />
+          <Route
+            path="contacts/:contactId"
+            element={<Contact />}
+            loader={contactLoader}
+            action={contactAction}
+          />
+          <Route
+            path="contacts/:contactId/edit"
+            element={<EditContact />}
+            loader={contactLoader}
+            action={editAction}
+          />
+          <Route
+            path="contacts/:contactId/destroy"
+            action={destroyAction}
+            errorElement={
+              <div>
+                Oops! There was an error deleting the user.{" "}
+                <Link to="/contacts/:contactId">Back to the contact</Link>
+              </div>
+            }
+          />
+        </Route>
       </Route>
     </DataBrowserRouter>
   </React.StrictMode>
--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,8 +1,21 @@
-import { Form, useLoaderData } from "react-router-dom";
-import { deleteContact, getContact } from "../contacts";
+import { Form, useFetcher, useLoaderData } from "react-router-dom";
+import { deleteContact, getContact, updateContact } from "../contacts";
 
 export async function loader({ params }) {
-  return getContact(params.contactId);
+  const contact = await getContact(params.contactId);
+
+  // Throw a Response to show a custom error.
+  if (!contact) {
+    throw new Response("", { status: 404, statusText: "Contact Not found" });
+  }
+  return contact;
+}
+
+export async function action({ request, params }) {
+  let formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
 }
 
 export default function Contact() {
@@ -58,10 +71,17 @@ export default function Contact() {
 }
 
 function Favorite({ contact }) {
+  const fetcher = useFetcher();
   // yes, this is a `let` for later
   let favorite = contact.favorite;
+
+  // To support optimistic UI, so we don't have to wait for the call to complete
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get("favorite") === "true";
+  }
+
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         name="favorite"
         value={favorite ? "false" : "true"}
@@ -69,6 +89,6 @@ function Favorite({ contact }) {
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 }
