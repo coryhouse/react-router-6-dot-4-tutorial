0f53f15e9bcbdfdf64dea2425b2a5b30f44c3ab2
Cory House <housecor@gmail.com>
2022-10-29 14:05:36 -0500

    upgrade

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { DataBrowserRouter, Link, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
 import "./index.css";
 import Root, {
   loader as rootLoader,
@@ -15,45 +20,49 @@ import Contact, {
 import EditContact, { action as editAction } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <DataBrowserRouter>
-      <Route
-        path="/"
-        element={<Root />}
-        errorElement={<ErrorPage />}
-        loader={rootLoader}
-        action={rootAction}
-      >
-        {/* Wrapping in a route with no path so we can declare a common error page 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      loader={rootLoader}
+      action={rootAction}
+      errorElement={<ErrorPage />}
+    >
+      {/* Wrapping in a route with no path so we can declare a common error page 
         for all the child routes below. 
         This way the left panel nav continues to display after an error. */}
-        <Route errorElement={<ErrorPage />}>
-          <Route index element={<Index />} />
-          <Route
-            path="contacts/:contactId"
-            element={<Contact />}
-            loader={contactLoader}
-            action={contactAction}
-          />
-          <Route
-            path="contacts/:contactId/edit"
-            element={<EditContact />}
-            loader={contactLoader}
-            action={editAction}
-          />
-          <Route
-            path="contacts/:contactId/destroy"
-            action={destroyAction}
-            errorElement={
-              <div>
-                Oops! There was an error deleting the user.{" "}
-                <Link to="/contacts/:contactId">Back to the contact</Link>
-              </div>
-            }
-          />
-        </Route>
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<Index />} />
+        <Route
+          path="contacts/:contactId"
+          element={<Contact />}
+          loader={contactLoader}
+          action={contactAction}
+        />
+        <Route
+          path="contacts/:contactId/edit"
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route
+          path="contacts/:contactId/destroy"
+          action={destroyAction}
+          errorElement={
+            <div>
+              Oops! There was an error deleting the user.{" "}
+              <Link to="/contacts/:contactId">Back to the contact</Link>
+            </div>
+          }
+        />
       </Route>
-    </DataBrowserRouter>
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
